423ecde0625cb348ad364b7e3e91f1c01d00aae5
BuildTools <unconfigured@null.spigotmc.org>
2019-10-04 17:21:45 +0200

    Ajout bouton + spring

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public SpriteRenderer[] door = new SpriteRenderer[2];
+    private Animator animator;
+    // Update is called once per frame
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool("IsTriggered", false);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        foreach(SpriteRenderer sr in door)
+        {
+            sr.enabled = true;
+        }
+        animator.SetBool("IsTriggered", true);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        foreach (SpriteRenderer sr in door)
+        {
+            sr.enabled = false;
+        }
+        animator.SetBool("IsTriggered", false);
+    }
+}
---
+++ Assets/Scripts/Spring.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.tag == "Player")
+        {
+            Rigidbody2D _rigidbody = collision.collider.gameObject.GetComponent<Rigidbody2D>();
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 20);
+            Debug.Log(collision.collider.gameObject.GetComponent<Rigidbody2D>().velocity);
+        }
+    }
+}
