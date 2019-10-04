2c0d1f40460cdb09481d96a51ee894ae6dc3cc75
Antony Herth <herth.antony@gmail.com>
2019-10-04 16:04:47 +0100

    mouvement player 1

---
+++ Assets/Mouvement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouvement : MonoBehaviour
+{
+    public float speed;
+    public float jumpforce;
+
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+
+    private Rigidbody2D _rigidbody;
+
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+        if (Input.GetKey(left))
+        {
+            _rigidbody.velocity = new Vector2(-speed, _rigidbody.velocity.y);
+        }
+        else if (Input.GetKey(right))
+        {
+            _rigidbody.velocity = new Vector2(speed, _rigidbody.velocity.y);
+        }
+        else
+        {
+            _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
+        }
+
+
+        if (Input.GetKey(jump) && isGrounded)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpforce);
+        }
+
+    }
+
+
+}
