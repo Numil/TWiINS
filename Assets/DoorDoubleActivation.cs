0ddaaef8ffb29ec61cf0e3e017b148f4a2fea362
BuildTools <unconfigured@null.spigotmc.org>
2019-10-18 16:58:23 +0200

    Ajout des Leviers

--- Assets/DoorDoubleActivation.cs
+++ Assets/DoorDoubleActivation.cs
@@ -7,18 +7,13 @@ public class DoorDoubleActivation : MonoBehaviour
     // Start is called before the first frame update
     public GameObject[] buttons;
     public SpriteRenderer[] openedDoor;
+    public SpriteRenderer[] closedDoor;
 
     private void Update()
     {
-        isOpen();
-    }
 
-    public bool isOpen()
-    {
         int countPressedButtons = 0;
-
-
-        foreach(GameObject button in buttons)
+        foreach (GameObject button in buttons)
         {
             if (button.GetComponent<SwitchActivated>().isSwitchOn)
             {
@@ -29,17 +24,26 @@ public class DoorDoubleActivation : MonoBehaviour
         {
             foreach (SpriteRenderer door in openedDoor)
             {
-                door.enabled = true;
+                door.gameObject.SetActive(true);
+            }
+            foreach (SpriteRenderer door in closedDoor)
+            {
+                door.gameObject.SetActive(false);
             }
         }
         else if (openedDoor[0].enabled != false)
         {
             foreach (SpriteRenderer door in openedDoor)
             {
-                door.enabled = false;
+                door.gameObject.SetActive(false);
+            }
+            foreach (SpriteRenderer door in closedDoor)
+            {
+                door.gameObject.SetActive(true);
             }
         }
-        return true;
+
     }
+
 
 }
