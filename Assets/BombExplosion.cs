9c4534cb7ecf3fd5ea851337c21ff41983843fa7
BuildTools <unconfigured@null.spigotmc.org>
2019-10-25 17:29:02 +0200

    Bombe

---
+++ Assets/BombExplosion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombExplosion : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public GameObject button;
+    private bool alreadyActivated = false;
+    public GameObject Explosion;
+
+    private void Update()
+    {
+        if (!alreadyActivated)
+        {
+            if (button.GetComponent<SwitchActivated>().isSwitchOn)
+            {
+                Instantiate(Explosion, transform.position, new Quaternion());
+                alreadyActivated = true;
+                Destroy(gameObject);
+            }
+        }
+        
+    }
+
+
+}
---
+++ Assets/Explosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+
+    Animation a;
+
+
+    private void Awake()
+    {
+        a = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (!a.isPlaying)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
