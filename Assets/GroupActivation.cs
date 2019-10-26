0c07d012e1db01647da0119a765b0a1dcf8ad3ef
BuildTools <florian.fornazaric@etu.unistra.fr>
2019-10-26 16:39:37 +0200

    Ajout d'activation/desactivation de bloc avec un bouton

--- Assets/GroupActivation.cs
+++ Assets/GroupActivation.cs
@@ -6,21 +6,66 @@ public class GroupActivation : MonoBehaviour
 {
 
     public GameObject lever;
+    public GameObject[] buttons;
     public GameObject GroupWhenOn;
     public GameObject GroupWhenOff;
+    public bool onlyOneActivation;
     // Update is called once per frame
-    void Update()
+    public bool alreadyActivated;
+
+    void Awake()
     {
-        if (lever.GetComponent<Lever>().isActivated)
+
+        if (buttons.Length != 0)
         {
             GroupWhenOff.SetActive(false);
             GroupWhenOn.SetActive(true);
         }
-        else
-        {
-            GroupWhenOff.SetActive(true);
-            GroupWhenOn.SetActive(false);
 
+        alreadyActivated = false;
+    }
+
+
+    void Update()
+    {
+        int countActivated = 0;
+
+        if ((!alreadyActivated && onlyOneActivation) || !onlyOneActivation)
+        {
+            if (lever != null)
+            {
+                if (lever.GetComponent<Lever>().isActivated)
+                {
+                    GroupWhenOff.SetActive(false);
+                    GroupWhenOn.SetActive(true);
+                }
+                else
+                {
+                    GroupWhenOff.SetActive(true);
+                    GroupWhenOn.SetActive(false);
+                }
+            }
+
+
+            if (buttons.Length != 0)
+            {
+
+                foreach (GameObject button in buttons)
+                {
+                    if (button.GetComponent<SwitchActivated>().isSwitchOn)
+                    {
+                        countActivated++;
+                    }
+                }
+                if (countActivated == buttons.Length)
+                {
+                    alreadyActivated = true;
+
+                    GroupWhenOff.SetActive(true);
+                    GroupWhenOn.SetActive(false);
+                }
+            }
         }
+        
     }
 }
